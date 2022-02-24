7599764c270cf39e8fd42c9788719773fb00fbe1
Caitano <josiasmartinscaitano@gmail.com>
2022-02-24 20:14:41 -0300

    adicionado o index.js

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,10 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
+  template: `
+    <div>
+    
+    </div>
+  `,
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
+
+  public string!: string;
 
   constructor() { }
 
